5d825f59c318b581d554f533e7ce01ee2f68c35c
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-09 20:21:24 +0530

    React Tic-Tac-Toe Game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
+   
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+    render() {
+        return (
+            <button className= "square" 
+            onClick={() => this.setState({value:'X'})}>
+                {this.state.value}
+            </button>
+        )
+    }
+}
+
+class Board extends React.Component{
+    renderSquare(i) {
+        return < Square value={i} />;
+    }
+}
