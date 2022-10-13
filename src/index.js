b2d108c99f74d799b549f1a391ef8149008cba91
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-13 19:07:03 +0530

    React Game

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-   
-
-
-
-
     render() {
         return (
             <button className= "square" 
@@ -72,6 +61,23 @@ class Board extends React.Component {
       );
     }
   }
+
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{}</div>
+            <ol>{}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
